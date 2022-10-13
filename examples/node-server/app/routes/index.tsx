e1d7527c6c0b23d2d283843f26d7b420b3049494
Marco De Gaetano <marco@degaetano.dev>
2022-10-13 23:43:45 +0200

    docs(node-server): :memo: expand example

--- examples/node-server/app/routes/index.tsx
+++ examples/node-server/app/routes/index.tsx
@@ -1,3 +1,40 @@
+import { Form, Link, useLoaderData } from '@remix-run/react'
+import { authenticator } from '~/services/auth.server'
+
+import type { ActionArgs, LoaderArgs } from '@remix-run/node'
+
 export default function Index() {
-  return <div>Test</div>
+  const user = useLoaderData<typeof loader>()
+  return (
+    <div>
+      <h1>Demo of remix-auth-strava-strategy</h1>
+      {!user && (
+        <div>
+          <h3>You are currently not logged in</h3>
+          <Link to='login'>Go to login page</Link>
+        </div>
+      )}
+      {user && (
+        <div>
+          <h3>
+            Hello {user.firstname} {user.lastname}
+          </h3>
+          <p>You are logged in</p>
+          {user.profile && <img src={user.profile} alt='profile' />}
+          <Form method='post' action='/?index'>
+            <button>Log out</button>
+          </Form>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export async function action({ request }: ActionArgs) {
+  await authenticator.logout(request, { redirectTo: '/' })
+}
+
+export async function loader({ request }: LoaderArgs) {
+  const user = await authenticator.isAuthenticated(request)
+  return user
 }
--- examples/node-server/app/services/auth.server.ts
+++ examples/node-server/app/services/auth.server.ts
@@ -1,8 +1,9 @@
 import { Authenticator } from 'remix-auth'
+import type { StravaProfile } from 'remix-auth-strava-strategy'
 import { StravaStrategy } from 'remix-auth-strava-strategy'
 import { sessionStorage } from './session.server'
 
-export const authenticator = new Authenticator(sessionStorage)
+export const authenticator = new Authenticator<StravaProfile>(sessionStorage)
 
 if (!process.env.STRAVA_CLIENT_ID || !process.env.STRAVA_CLIENT_SECRET) {
   throw new Error('You must set STRAVA_CLIENT_ID and STRAVA_CLIENT_SECRET')
@@ -13,16 +14,16 @@ authenticator.use(
     {
       clientID: process.env.STRAVA_CLIENT_ID,
       clientSecret: process.env.STRAVA_CLIENT_SECRET,
-      redirectURI: '/auth/strava/callback',
+      redirectURI: 'localhost:3000/auth/strava/callback',
       approvalPrompt: 'force',
       scope: ['read'],
     },
     async ({ accessToken, extraParams, profile, refreshToken, context }) => {
-      console.log(accessToken)
-      console.log(extraParams)
-      console.log(profile)
-      console.log(refreshToken)
-      console.log(context)
+      // console.log(accessToken)
+      // console.log(extraParams)
+      // console.log(profile)
+      // console.log(refreshToken)
+      // console.log(context)
       return profile
     }
   )
