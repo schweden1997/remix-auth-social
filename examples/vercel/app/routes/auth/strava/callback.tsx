11fb1ad77b255f076c43a9d5a5441b630f33a65b
Marco De Gaetano <marco@degaetano.dev>
2022-10-14 18:17:25 +0200

    chore(monorepo): :hammer: remove faulty vercel example

--- examples/vercel/app/routes/auth/strava/callback.tsx
+++
@@ -1,8 +0,0 @@
-import type { LoaderArgs } from '@remix-run/node'
-import { authenticator } from '~/services/auth.server'
-export async function loader({ request }: LoaderArgs) {
-  return authenticator.authenticate('strava', request, {
-    successRedirect: '/',
-    failureRedirect: '/login',
-  })
-}
--- examples/vercel/app/routes/login.tsx
+++
@@ -1,9 +0,0 @@
-import { Form } from '@remix-run/react'
-
-export default function Login() {
-  return (
-    <Form action='/auth/strava' method='post'>
-      <button>Login with Strava</button>
-    </Form>
-  )
-}
